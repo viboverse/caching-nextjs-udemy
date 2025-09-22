e5b28c746be586a64216d4b8060f89243fd26927
vibo <vahab.afsharian94@gmail.com>
2025-09-22 14:02:45 +0300

    Refactor message handling to use local data retrieval and improve code clarity

--- app/messages/new/page.js
+++ app/messages/new/page.js
@@ -1,14 +1,16 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
-import { addMessage } from '@/lib/messages';
+import { addMessage } from "@/lib/messages";
+import { revalidate } from "../page";
 
 export default function NewMessagePage() {
   async function createMessage(formData) {
-    'use server';
+    "use server";
 
-    const message = formData.get('message');
+    const message = formData.get("message");
     addMessage(message);
-    redirect('/messages');
+    // revalidate();
+    redirect("/messages");
   }
 
   return (
--- app/messages/page.js
+++ app/messages/page.js
@@ -1,12 +1,14 @@
-import Messages from '@/components/messages';
+import Messages from "@/components/messages";
+import { getMessages } from "@/lib/messages";
+
+// export const revalidate = 5;
+// export const dynamic = "force-dynamic";
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
-  });
-  const messages = await response.json();
+  // const response = await fetch("http://localhost:8080/messages");
+  // const messages = await response.json();
+
+  const messages = getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
